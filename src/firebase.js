88bf44342c6ddf4b93cfdc7d0c0a308beed49653
TELI MUSHERA <telimushera10@gmail.com>
2025-09-17 01:29:59 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
 // Import the functions you need from the SDKs you need
-//
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries // Your web app's Firebase configuration // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Add SDKs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#available-libraries 
+// Your web app's Firebase configuration 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { OAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
